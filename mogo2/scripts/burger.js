3b0c5d1dd21037e27d886b1015ccfd6ee18bc9d0
EyeSeeIce <71644947+EyeSeeIce@users.noreply.github.com>
2020-09-25 21:59:31 +0300

    qwe

---
+++ mogo2/scripts/burger.js
@@ -0,0 +1,28 @@
+//jQerry(document).ready(function(){
+//  $(".menu").on('click',function(){
+//    $(this).find(".hambergerIcon").toggleClass("open");
+//  });
+//})
+//
+
+//const menu = document.querySelector('.menu')
+//		burger = document.querySelector('.hambergerIcon')
+//
+//menu.addEventListener('click', open);
+//function open (argument) {
+//	burger.classList.toggle('open')
+//}
+
+//$('button').on('click', function() {
+//  $(this).toggleClass('is-active');
+//});
+
+
+	document.querySelector('.menu-toggle').addEventListener('click', open)
+
+function open () {
+		this.classList.toggle('active')
+}
+
+
+
