1ddb23b2d1ec4bab738d474f6cdb3898d483acdd
EyeSeeIce <71644947+EyeSeeIce@users.noreply.github.com>
2020-11-10 18:32:50 +0300

    qwe

--- webAr/js.js
+++ webAr/js.js
@@ -1,7 +1,10 @@
 let me = document.querySelector('#me')
-let r = 0;
+let me2 = document.querySelector('#me2')
+/*let r = 0;
 setInterval(() => {
   r++
   me.setAttribute('rotation', r+' '+'-70 90')
 }, 30)
+
+*/
 
